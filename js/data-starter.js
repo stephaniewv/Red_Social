1a2193e4514977f36522ee6048b60dacce820968
Yelitza3125 <deyel_99@hotmail.com>
2018-01-09 18:21:41 -0500

    Maquetación y estilo de la vista del perfil

---
+++ js/data-starter.js
@@ -0,0 +1,4 @@
+var imgStarter = ['st-1.jpg', 'st-2.jpg', 'st-3.jpg', 'st-4.jpg', 'st-5.jpg', 'st-6.jpg'];
+
+
+var imgGallery = ['gal-1.jpg', 'gal-2.jpg', 'gal-3.jpg', 'gal-4.jpg', 'gal-5.jpg', 'gal-6.jpg'];
--- js/profile.js
+++ js/profile.js
@@ -1,9 +1,69 @@
 $(document).ready(function() {
 
-var inputName = localStorage.getItem('name');
-$('#pg-name').html(inputName);
+  var inputName = localStorage.getItem('name');
+  $('#pg-name').html(inputName);
+
+  $('#textarea1').trigger('autoresize');
+ 
+  var imgSta = imgStarter;
+  console.log(imgSta);
+  var $imgSt = $('<img>', {
+    'class': 'responsive-img',
+  });
 
 
-$('#textarea1').trigger('autoresize');
+  $('.btn-st').click(function() {
+  
+    for(var i = 0; i<imgSta.length; i++) {
+       $imgSt.attr('src', '../assets/images/starter' + '/' + imgSta[i]);
+       $('.gal-img', {
+        'id': [i]
+       });
+      console.log(i);
+    }
+
+    //console.log(i);
+    //console.log($imgSt);
+    $('#'[i]).replaceWith($imgSt);   
+  })
+
+  $('.btn-gal').click(function() {
+    $imgSt.hide();
+  })
+
+  $('.btn-floating').click(function() {
+    event.preventDefault();
+    var $imgProfile = $('<img>', {
+      'class': 'activator',
+      'src': '../assets/images/card-profile.jpg'
+    })
+
+    $('.card-image').append($imgProfile);
+    $('.btn-floating').detach();
+  })
+
+  $('.btn-post').click(function() {
+    event.preventDefault();
+    newCard();
+  })
+  
+  function newCard() {
+
+    var span = $('<span class = "card-title activator grey-text text-darken-4">Mi Mundo</span>');
+    var anchor = $('<a class= "btn-floating halfway-fab waves-effect waves-light red" ><i class= "material-icons" >add</i></a>');
+    var textarea = $('<textarea id="textarea1" class= "materialize-textarea" placeholder= "¿Qué vas a crear hoy?"></textarea>')
+    
+    var container = $('<div class = "card-content"></div>');
+    var card = $('<div class = "card col l12" ></div>');
+    var cardImage = $('<div class= "card-image waves-effect waves-block waves-light"></div>');
+    var bigContainer = $('.card-container');
+    var btn = $('<button class="btn waves-effect waves-light btn-post" type="button" name="action">Crear<i class="material-icons right">border_color</i></button>');
+    container.append(span, anchor, textarea, btn);
+    card.append(cardImage, container);
+    bigContainer.prepend(card);
+  }
+
+  $('.history-name').html(inputName);
 
 });
+
