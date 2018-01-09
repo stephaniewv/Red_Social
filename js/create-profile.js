d1fd49367a9aaa04bee02c20384d9f15c944b381
Yelitza3125 <deyel_99@hotmail.com>
2018-01-09 00:18:51 -0500

    Añadiendo plugin para funcionalidad de cargado de imagen

--- js/create-profile.js
+++ js/create-profile.js
@@ -1,4 +1,37 @@
 $(document).ready(function() {
     $('select').material_select();
+
+/*
+    $("#file-3").fileinput({
+        showCaption: false,
+        browseClass: "btn btn-primary btn-lg",
+        fileType: "any"
+        }); */
+        
+        var btnCust = '<button type="button" class="btn btn-secondary" title="Add picture tags" ' + 
+        'onclick="alert(\'Call your custom code here.\')">' +
+        '<i class="glyphicon glyphicon-tag"></i>' +
+        '</button>'; 
+
+    $("#avatar-1").fileinput({
+        overwriteInitial: true,
+        maxFileSize: 5000,
+        showClose: false,
+        showCaption: false,
+        browseLabel: '',
+        removeLabel: '',
+        browseIcon: '<i class="material-icons">add</i>',
+        removeIcon: '<i class="material-icons">delete</i>',
+        removeTitle: 'Cancel or reset changes',
+        elErrorContainer: '#kv-avatar-errors-1',
+        msgErrorClass: 'alert alert-block alert-danger',
+        defaultPreviewContent: '<img src="/uploads/default_avatar_male.jpg" alt="Your Avatar">',
+        layoutTemplates: {main2: '{preview} ' +  btnCust + ' {remove} {browse}'},
+        allowedFileExtensions: ["jpg", "png", "gif"]
+    });
+      
+    
+
   });
-            
+
+ 
