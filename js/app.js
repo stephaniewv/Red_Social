f9cb7de042ece3cbc54c430993de21f2902da7da
stephaniewv <stephaniewv@gmail.com>
2018-01-14 17:18:01 -0500

    upload js firebase

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
-$(document).ready(function(){
-    setTimeout(function(){
-        window.location.href='views/home.html';
-    },3000);
+$(document).ready(function () {
+setTimeout(function () {
+	window.location.href = 'views/home.html';
+}, 5000);
+$("#typed").typed({
+	strings: [";)", "Bienvenido a tu nuevo mundo"],
+	typeSpeed: 30,
+	backDelay: 500,
+	loop: false,
+	loopCount: false,
+	callback: function () { foo(); }
 });
+function foo() { console.log("Callback"); }
+});
+
+
---
+++ vendors/js/firebaseapp.js
@@ -0,0 +1,9 @@
+var config = {
+      apiKey: "AIzaSyDtsAjvNtidTzvESlO5b3dTSd_pBEsbfqE",
+      authDomain: "rolling-laboratoria.firebaseapp.com",
+      databaseURL: "https://rolling-laboratoria.firebaseio.com",
+      projectId: "rolling-laboratoria",
+      storageBucket: "rolling-laboratoria.appspot.com",
+      messagingSenderId: "227778674041"
+    };
+    firebase.initializeApp(config);
