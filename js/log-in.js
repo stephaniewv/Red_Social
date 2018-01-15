0e233e2395689fe1c5af18d79d0912ff88b65f8d
stephaniewv <stephaniewv@gmail.com>
2018-01-15 14:36:55 -0500

    Change redirecting

--- js/log-in.js
+++ js/log-in.js
@@ -1,24 +1,5 @@
 $(document).ready(function () {
-  // console.log(JSON.parse(localStorage.getItem('email_new_user'))); // array
-
-  // /* Recuperando la información guardada en los array */  
-  // var emailArray = JSON.parse(localStorage.getItem('email_new_user'));
-  // var passwordArray = JSON.parse(localStorage.getItem('password_new_user'));
-
-  // /* Comprobando que el email sea el mismo */
-  // function email() {
-  //   for (var i = 0; i < emailArray.length; i++) {
-  //     return $('#email-log-in').val() === emailArray[i];
-  //   }
-  // } 
-
-  // /* Comprobando que la contraseña sea la misma */
-  // function password() {
-  //   for (var i = 0; i < passwordArray.length; i++) {
-  //     return $('#password-log-in').val() === passwordArray[i];
-  //   }     
-  // }
-
+  
   /* Si ambos campos son correctos, se habilita el botón */
   $('#password-log-in').keyup(function allOk() {
     var email = $('#email-log-in').val();
@@ -37,7 +18,7 @@ $(document).ready(function () {
     var password = $('#password-log-in').val();
     firebase.auth().signInWithEmailAndPassword(email, password).then(function (result) {
       console.log(result);
-      window.location = '/views/inicio.html';
+      window.location = '/views/profile.html';
 
     }).catch(function (error) {
       console.log(error.code);
